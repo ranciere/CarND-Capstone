8a0b3a8dcb2bc87a462f6c0ac49943605719b04d
ranciere <39014565+ranciere@users.noreply.github.com>
2021-01-23 20:45:28 +0100

    Some Business Logic

--- ros/src/tl_detector/light_classification/tl_classifier_site.py
+++ ros/src/tl_detector/light_classification/tl_classifier_site.py
@@ -29,9 +29,16 @@ def load_mobilenet(fname):
 #~ light_types['GREEN_YELLOW'] = [0,0,0,0,0,1,0]
 #~ light_types['NO_LIGHT']     = [0,0,0,0,0,0,1]
 
+trafficlight_str = {}
+trafficlight_str[0] = 'RED'
+trafficlight_str[1] = 'YELLOW'
+trafficlight_str[2] = 'GREEN'
+trafficlight_str[4] = 'UNKNOWN'
+
 class TLClassifierSite(object):
     def __init__(self):
         self.model = load_mobilenet('light_classification/mobilenet_model.h5')
+        self.prev_pred = TrafficLight.UNKNOWN
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -59,6 +66,32 @@ class TLClassifierSite(object):
             res = TrafficLight.YELLOW
         elif pred[6] > 0.8:
             res = TrafficLight.UNKNOWN
+        # Complex cases
         else:
-            res = TrafficLight.UNKNOWN
+            res = self.complex_cases(pred)
+        self.prev_pred = res
+        
+        return res
+
+    def complex_cases(self, pred):
+        res = TrafficLight.UNKNOWN
+        # Based on previous VALID light
+        if self.prev_pred == TrafficLight.RED:
+            # Only RED -> RED, the RED -> GREEN alternation is possbile, but it is a defensive algorithm
+            if pred[0] > 0.5:
+                res = TrafficLight.RED
+        elif self.prev_pred == TrafficLight.GREEN:
+            # GREEN -> GREEN
+            if pred[1] > 0.5:
+                res = TrafficLight.GREEN
+            # GREEN -> YELLOW
+            elif pred[2] > 0.5:
+                res = TrafficLight.YELLOW
+        elif self.prev_pred == TrafficLight.YELLOW:
+            # YELLOW -> YELLOW
+            if pred[2] > 0.5:
+                res = TrafficLight.YELLOW
+            # YELLOW -> RED
+            elif pred[0] > 0.5:
+                res = TrafficLight.RED
         return res
