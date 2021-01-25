8220b6f0e01cd64c2002d4c25b40b33c984e56ad
kovacsre <reka.horvath.kovacs@gmail.com>
2021-01-25 13:37:26 -0800

    Update tl_classifier.py
    Getting rid of false detections and decreasing color count thresholds.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -26,30 +26,30 @@ class TLClassifier(object):
 	#cut off lower 1/3 of the image where traffic lights cannot be		
 	img_crop = img_hsv[0:400,0:800] 
 	#create a binary mask for red color. note, that red is at the beginning and at the end of the H circle therefore 2 masks are needed
-	mask1r = cv2.inRange(img_crop, (0,50,20), (5,255,255))
-	mask2r = cv2.inRange(img_crop, (175,50,20), (180,255,255))
+	mask1r = cv2.inRange(img_crop, (0,200,200), (5,255,255))
+	mask2r = cv2.inRange(img_crop, (175,200,200), (180,255,255))
 	#combine the 2 masks
 	maskr = cv2.bitwise_or(mask1r,mask2r)
 	#yellow mask, S (saturation) value is set very high to select 'very yellow' pixels only -there may be much yellow in the background hills with low saturation
 	masky = cv2.inRange(img_crop, (26,200,200), (32,255,255))
 	#green mask, same applies here as for the yellow mask: high saturation green is selected to be able to tell traffic lights from trees
-	maskg = cv2.inRange(img_crop, (41,200,20), (67,255,255))
+	maskg = cv2.inRange(img_crop, (41,200,200), (67,255,255))
 	
 	num_pixels = np.array([0,0,0]) #number of red, yellow, green pixels in this order
 	rcnt = cv2.countNonZero(maskr)
 	ycnt = cv2.countNonZero(masky)
 	gcnt = cv2.countNonZero(maskg)
-	if rcnt > 500:
+	if rcnt > 30:
 	    num_pixels[0] = rcnt
 	else: 
 			num_pixels[0] = 0
 	
-	if ycnt > 500:
+	if ycnt > 30:
 	    num_pixels[1] = ycnt
 	else: 
 			num_pixels[1] = 0
 	
-	if gcnt > 500:
+	if gcnt > 30:
 	    num_pixels[2] = gcnt
 	else: 
 			num_pixels[2] = 0
